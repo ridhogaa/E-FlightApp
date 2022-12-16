658da188881677c3b604d62a88db921cd79e089a
Honvax <111961739+Honvax@users.noreply.github.com>
2022-12-16 20:15:49 +0700

    Worker,Utils,User Profile

--- app/src/main/java/com/c10/finalproject/data/local/datastore/DataStoreManager.kt
+++ app/src/main/java/com/c10/finalproject/data/local/datastore/DataStoreManager.kt
@@ -26,16 +26,37 @@ class DataStoreManager(@ApplicationContext private val context: Context) {
             it[TOKEN_USER_KEY] = token
         }
     }
+    suspend fun setImage(image: String){
+        context.dataStore.edit {
+            it[IMAGE] = image
+        }
+    }
+
+    suspend fun statusLogin(isLogin: Boolean) {
+        context.dataStore.edit {
+            it[LOGIN_STATUS] = isLogin
+        }
+    }
 
     suspend fun clearToken() {
         context.dataStore.edit {
             it.clear()
         }
     }
+    fun getImage(): Flow<String> {
+        return context.dataStore.data.map {
+            it[IMAGE] ?: ""
+        }
+    }
+
 
     companion object {
         private const val DATASTORE_NAME = "datastore_preferences"
         private val TOKEN_USER_KEY = stringPreferencesKey("token_user_key")
+        private val IMAGE = stringPreferencesKey("image_key")
+        private val LOGIN_STATUS = booleanPreferencesKey("login_status_key")
+
+
         private val Context.dataStore by preferencesDataStore(
             name = DATASTORE_NAME
         )
--- app/src/main/java/com/c10/finalproject/ui/user/profile/ProfileFragment.kt
+++ app/src/main/java/com/c10/finalproject/ui/user/profile/ProfileFragment.kt
@@ -1,42 +1,188 @@
 package com.c10.finalproject.ui.user.profile
 
+
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.c10.finalproject.R
 import com.c10.finalproject.databinding.FragmentProfileBinding
-import com.c10.finalproject.ui.MainActivity
+import com.c10.finalproject.utils.Utils
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.ByteArrayOutputStream
+
+
+const val REQUEST_CODE_PERMISSION = 201
+@Suppress("DEPRECATION")
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
-
-    private val viewModel: ProfileViewModel by activityViewModels()
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private val profileViewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the layout for this fragment
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        logout()
+
+        binding.btnChangeImage.setOnClickListener { checkingPermission() }
+        binding.btnUpdate.setOnClickListener { toUpdateAccount() }
+        binding.btnLogout.setOnClickListener { toLogout() }
+
+        profileViewModel.getImage().observe(viewLifecycleOwner) {
+            if (it.isNullOrEmpty().not()) {
+                setProfilePicture(Utils.convertStringToBitmap(it))
+            } else {
+                binding.imageUser.setImageResource(R.drawable.ic_photo_profile)
+            }
+        }
     }
 
-    private fun logout() {
-        binding.btnLogout.setOnClickListener {
-            viewModel.clearToken()
-            startActivity(Intent(requireContext(), MainActivity::class.java))
-            activity?.finish()
+    private fun setProfilePicture(bitmap: Bitmap) {
+        binding.imageUser.load(bitmap) {
+            crossfade(true)
+            placeholder(R.drawable.ic_photo_profile)
+            transformations(RoundedCornersTransformation())
         }
+    }
+
+    private fun checkingPermission() {
+        if(isGranted(
+                requireActivity(), Manifest.permission.CAMERA,
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.MANAGE_EXTERNAL_STORAGE
+                ), REQUEST_CODE_PERMISSION
+            )
+        ){
+            chooseImageDialog()
+        }
+    }
+
+    private fun chooseImageDialog() {
+        AlertDialog.Builder(requireContext())
+            .setMessage("Choose Picture")
+            .setPositiveButton("Gallery") { _, _ -> openGallery()}
+            .show()
+    }
+
+    private fun isGranted(
+        activity: Activity,
+        permission: String,
+        permissions: Array<String>,
+        request: Int
+    ): Boolean {
+        val permissionCheck = ActivityCompat.checkSelfPermission(activity, permission)
+        return if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                showPermissionDeniedDialog()
+            } else {
+                ActivityCompat.requestPermissions(activity, permissions, request)
+            }
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun openGallery() {
+        requireActivity().intent.type = "image/*"
+        galleryResult.launch("image/*")
+    }
+
+    private val galleryResult =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { result ->
+            binding.imageUser.load(result) {
+                crossfade(true)
+                placeholder(R.drawable.ic_photo_profile)
+                target { result ->
+                    val bitmap = (result as BitmapDrawable).bitmap
+                    profileViewModel.uploadImage(Utils.convertBitmapToString(bitmap))
+                    profileViewModel.applyBlur(getImageUri(bitmap))
+                }
+                transformations(RoundedCornersTransformation())
+            }
+        }
+
+    private fun getImageUri(bitmap: Bitmap): Uri {
+        val bytes = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path: String = MediaStore.Images.Media.insertImage(
+            requireContext().contentResolver,
+            bitmap,
+            "Initial Profile Picture",
+            null
+        )
+        return Uri.parse(path)
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Permission Denied")
+            .setMessage("Permission is Denied, pleas Allow Permission from settings")
+            .setPositiveButton(
+                "App Settings"
+            ) { _, _ ->
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+            .show()
+    }
+
+    private fun toUpdateAccount() {
+        val cardNumber = binding.etCardNumber.text.toString()
+        val username = binding.etUsername.text.toString()
+        val email = binding.etEmail.text.toString()
+        val name = binding.etName.text.toString()
+        val contact = binding.etContact.text.toString()
+        val birthday = binding.etBirthday.text.toString()
+        val address = binding.etAddress.text.toString()
+
+        profileViewModel.editAccount(cardNumber,username,email,name,contact,birthday,address)
+        Toast.makeText(requireContext(), "Update Success", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_profileFragment_to_homeFragment)
+    }
+
+    private fun toLogout() {
+        val option = NavOptions.Builder()
+            .setPopUpTo(R.id.profileFragment, true)
+            .build()
+
+        profileViewModel.statusLogin(false)
+        findNavController().navigate(R.id.action_profileFragment_to_loginFragment, null, option)
+
     }
 
     override fun onDestroy() {
